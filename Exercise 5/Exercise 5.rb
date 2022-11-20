c88504d51671590d54a4ce3b9ab2faf0cafaff7f
ehjohnson154 <spiffy154@gmail.com>
2022-11-20 10:39:35 -0800

    changing name

--- Exercise 5/Exercise 5.rb
+++ Exercise 5/Exercise 5.rb
@@ -4,42 +4,52 @@
 
 class LaunchDiscussionWorkflow
 
-    def initialize(discussion, host, participants_email_string)
+    def initialize(discussion, host, users)
       @discussion = discussion
       @host = host
-      @participants_email_string = participants_email_string
-      @participants = []
+      @participants = users
     end
   
     # Expects @participants array to be filled with User objects
     def run
       return unless valid?
       run_callbacks(:create) do
-        ActiveRecord::Base.transaction do #//Do acts as "curly braces"
-          discussion.save! #// ! means its going to modify data
+        ActiveRecord::Base.transaction do
+          discussion.save! 
           create_discussion_roles!
           @successful = true
         end
       end
     end
-  
-    def generate_participant_users_from_email_string
-      return if @participants_email_string.blank? #// going to return true or false
-      @participants_email_string.split.uniq.map do |email_address|
-        User.create(email: email_address.downcase, password: Devise.friendly_token)
-      end
-    end
-  
     # ...
   
   end
+
+#new class participants:
+#-pass in list of emails
+#-generate participant list
+#-stores list of participants for other classes to call
+class Participants
+
+  def initialize(participants_list)
+    @participants = []
+
+  def generate_participant_users
+    return if participant_list.blank?
+    participants_list do |email_address| #iterate through list, append participants with new user objects
+      @participants.push(User.create(email: email_address.downcase, password: Devise.friendly_token))
+    end
+  end
+
+end
   
   
   discussion = Discussion.new(title: "fake", ...)
   host = User.find(42)
-  participants = "fake1@example.com\nfake2@example.com\nfake3@example.com" #// why arew we giving a long string? why not a array?
-                                                                            #// why not make a add user method?
-  
-  workflow = LaunchDiscussionWorkflow.new(discussion, host, participants)
-  workflow.generate_participant_users_from_email_string
+
+  participant_users = Participants.new(["fake1@example.com","fake2@example.com","nfake3@example.com"])
+  participant_users.generate_participant_users()
+  participant_list =  Participants.participants
+
+  workflow = LaunchDiscussionWorkflow.new(discussion, host, participant_list)
   workflow.run
